2e809f96383916db119d54ea98d15b500699bceb
jooyoung <hjoo820@naver.com>
2024-07-04 09:04:55 +0900

    Applied review feedback

--- Project11/Similarity.cpp
+++ Project11/Similarity.cpp
@@ -6,23 +6,15 @@ class StringSimilarityChecker {
 public:
 	const int MAX_SCORE = 60;
 	int run(string a, string b) {
-		if (isSameLength(a, b)) { return MAX_SCORE; }
-		if (isDoubleLength(a, b)) { return 0; }
-		return GetScore(a, b);
-	}
-	int GetScore(std::string& a, std::string& b)
-	{
 		int longlen = max(a.length(), b.length());
 		int shortlen = min(a.length(), b.length());
+
+		if (longlen == shortlen) { return MAX_SCORE; }
+		if (longlen == shortlen * 2) { return 0; }
+		return GetScore(longlen, shortlen);
+	}
+	int GetScore(int longlen, int shortlen)
+	{
 		return MAX_SCORE * (shortlen - (longlen - shortlen)) / shortlen;
-	}
-	bool isSameLength(std::string& a, std::string& b)
-	{
-		return (a.length() == b.length());
-	}
-	bool isDoubleLength(std::string& a, std::string& b)
-	{
-		return (a.length() * 2 <= b.length()
-			|| a.length() >= b.length() * 2);
 	}
 };
