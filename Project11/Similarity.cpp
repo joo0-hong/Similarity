88895d36b2df386e9cbe443b77849a90f62776c9
jooyoung <hjoo820@naver.com>
2024-07-04 10:14:52 +0900

    Get Alphabet score

--- Project11/Similarity.cpp
+++ Project11/Similarity.cpp
@@ -1,24 +1,78 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 class StringSimilarityChecker {
 public:
-	const int MAX_SCORE = 60;
-	int run(string a, string b) {
+	const int MAX_LENGTH_SCORE = 60;
+	const int MAX_ALPHA_SCORE = 40;
+	int getLengthScore(string a, string b) {
 		int longlen = max(a.length(), b.length());
 		int shortlen = min(a.length(), b.length());
 
 		if (longlen == shortlen) {
-			return MAX_SCORE;
+			return MAX_LENGTH_SCORE;
 		}
 		if (longlen >= shortlen * 2) {
 			return 0;
 		}
-		return GetScore(longlen, shortlen);
+		return calcLengthScore(longlen, shortlen);
 	}
-	int GetScore(int longlen, int shortlen)
+	int calcLengthScore(int longlen, int shortlen)
 	{
-		return MAX_SCORE * (shortlen - (longlen - shortlen)) / shortlen;
+		return MAX_LENGTH_SCORE * (shortlen - (longlen - shortlen)) / shortlen;
 	}
+	int getAlphaScore(string a, string b)
+	{
+		for (int alen = 0; alen < a.length(); alen++) {
+			addCharTotalList(a[alen]);
+			for (int blen = 0; blen < b.length(); blen++) {
+				addCharTotalList(b[blen]);
+				if (a[alen] == b[blen])
+				{
+					addCharToSameList(a[alen]);
+				}
+			}
+		}
+
+		if (sameList.size() == 0)
+			return 0;
+
+		if (a.length() == sameList.size())
+			return MAX_ALPHA_SCORE;
+
+		return MAX_ALPHA_SCORE * sameList.size() / totalList.size();
+	}
+
+	void addCharToSameList(char sameChar)
+	{
+		bool found = false;
+		for (char ch : sameList)
+		{
+			if (ch == sameChar) {
+				found = true;
+				break;
+			}
+		}
+		if (found == false)
+			sameList.push_back(sameChar);
+	}
+
+	void addCharTotalList(char input)
+	{
+		bool found = false;
+		for (char ch : totalList)
+		{
+			if (ch == input) {
+				found = true;
+				break;
+			}
+		}
+		if (found == false)
+			totalList.push_back(input);
+	}
+
+	vector<char> totalList;
+	vector<char> sameList;
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -9,21 +9,21 @@ TEST(TestCaseName, SameLength) {
 	string a = "ASD";
 	string b = "DSA";
 	
-	EXPECT_EQ(checker.run(a, b), 60);
+	EXPECT_EQ(checker.getLengthScore(a, b), 60);
 }
 TEST(TestCaseName, DoubleLength) {
 	StringSimilarityChecker checker;
 	string a = "AA";
 	string b = "B";
 	
-	EXPECT_EQ(checker.run(a, b), 0);
+	EXPECT_EQ(checker.getLengthScore(a, b), 0);
 }
 TEST(TestCaseName, Length5and3) {
 	StringSimilarityChecker checker;
 	string a = "AAABB";
 	string b = "BAA";
 	
-	EXPECT_EQ(checker.run(a, b), 20);
+	EXPECT_EQ(checker.getLengthScore(a, b), 20);
 }
 
 TEST(TestCaseName, Including) {
@@ -31,5 +31,38 @@ TEST(TestCaseName, Including) {
 	string a = "AA";
 	string b = "AAE";
 	
-	EXPECT_EQ(checker.run(a, b), 30);
+	EXPECT_EQ(checker.getLengthScore(a, b), 30);
 }
+
+TEST(TestCaseName, SameAlphabets) {
+	StringSimilarityChecker checker;
+	string a = "ASD";
+	string b = "DSA";
+
+	EXPECT_EQ(checker.getAlphaScore(a, b), 40);
+}
+
+TEST(TestCaseName, NoneSameAlphabets) {
+	StringSimilarityChecker checker;
+	string a = "A";
+	string b = "BB";
+
+	EXPECT_EQ(checker.getAlphaScore(a, b), 0);
+}
+
+TEST(TestCaseName, AlphabetIncluding) {
+	StringSimilarityChecker checker;
+	string a = "AAABB";
+	string b = "BA";
+
+	EXPECT_EQ(checker.getAlphaScore(a, b), 40);
+}
+
+TEST(TestCaseName, AlphabetIncluding2) {
+	StringSimilarityChecker checker;
+	string a = "AA";
+	string b = "AAE";
+
+	EXPECT_EQ(checker.getAlphaScore(a, b), 20);
+}
+
