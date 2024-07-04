a6ad9a24443427e924f75150d7ee04c10df364b8
jooyoung <hjoo820@naver.com>
2024-07-04 09:09:20 +0900

    Bug fix

--- Project11/Similarity.cpp
+++ Project11/Similarity.cpp
@@ -10,7 +10,7 @@ public:
 		int shortlen = min(a.length(), b.length());
 
 		if (longlen == shortlen) { return MAX_SCORE; }
-		if (longlen == shortlen * 2) { return 0; }
+		if (longlen >= shortlen * 2) { return 0; }
 		return GetScore(longlen, shortlen);
 	}
 	int GetScore(int longlen, int shortlen)
